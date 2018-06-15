7027ef6ee3ed888e52ae0174cc564266eed471bf
hema <egy.kid1@gmail.com>
2018-06-15 02:38:39 +0200

    Go To Goal Algorithm

--- scripts/go_to_point.py
+++ scripts/go_to_point.py
@@ -1,31 +1,40 @@
 #! /usr/bin/env python
+
+# import ros stuff
 import rospy
 from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist, Point
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Twist,Point
 from tf import transformations
+
 import math
 
-position_=Point()
-yaw_=0
-state_=0
-
-desired_position=Point()
-desired_position.x=-3
-desired_position.y=7
-desired_position.z=0
-yaw_precision_ = math.pi / 90
+# robot state variables
+position_ = Point()
+yaw_ = 0
+# machine state
+state_ = 0
+# goal
+desired_position_ = Point()
+desired_position_.x = -3
+desired_position_.y = 7
+desired_position_.z = 0
+# parameters
+yaw_precision_ = math.pi / 90 # +/- 2 degree allowed
 dist_precision_ = 0.3
-pub=None
 
+# publishers
+pub = None
 
-
+# callbacks
 def clbk_odom(msg):
     global position_
     global yaw_
-
-    position_=msg.pose.pose.position
-
+    
+    # position
+    position_ = msg.pose.pose.position
+    
+    # yaw
     quaternion = (
         msg.pose.pose.orientation.x,
         msg.pose.pose.orientation.y,
@@ -34,11 +43,16 @@ def clbk_odom(msg):
     euler = transformations.euler_from_quaternion(quaternion)
     yaw_ = euler[2]
 
+def change_state(state):
+    global state_
+    state_ = state
+    print 'State changed to [%s]' % state_
 
 def fix_yaw(des_pos):
     global yaw_, pub, yaw_precision_, state_
     desired_yaw = math.atan2(des_pos.y - position_.y, des_pos.x - position_.x)
     err_yaw = desired_yaw - yaw_
+    
     twist_msg = Twist()
     if math.fabs(err_yaw) > yaw_precision_:
         twist_msg.angular.z = 0.7 if err_yaw > 0 else -0.7
@@ -47,17 +61,8 @@ def fix_yaw(des_pos):
     
     # state change conditions
     if math.fabs(err_yaw) <= yaw_precision_:
-        print ('Yaw error: [%s]' % err_yaw)
+        print 'Yaw error: [%s]' % err_yaw
         change_state(1)
-
-
-def change_state(state):
-    global state_
-    state_ = state
-    print ('State changed to [%s]' % state_)
-
-
-
 
 def go_straight_ahead(des_pos):
     global yaw_, pub, yaw_precision_, state_
@@ -70,12 +75,12 @@ def go_straight_ahead(des_pos):
         twist_msg.linear.x = 0.6
         pub.publish(twist_msg)
     else:
-        print ('Position error: [%s]' % err_pos)
+        print 'Position error: [%s]' % err_pos
         change_state(2)
     
     # state change conditions
     if math.fabs(err_yaw) > yaw_precision_:
-        print ('Yaw error: [%s]' % err_yaw)
+        print 'Yaw error: [%s]' % err_yaw
         change_state(0)
 
 def done():
@@ -84,30 +89,28 @@ def done():
     twist_msg.angular.z = 0
     pub.publish(twist_msg)
 
-
-
-
 def main():
     global pub
+    
     rospy.init_node('go_to_point')
-    pub=rospy.Publisher('/cmd/vel',Twist,queue_size=1)
-    sub=rospy.Subscriber('/odom',Odometry,clbk_odom)
-    rate=rospy.Rate(20)
+    
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+    
+    sub_odom = rospy.Subscriber('/odom', Odometry, clbk_odom)
+    
+    rate = rospy.Rate(20)
     while not rospy.is_shutdown():
-        if state_==0:
-            fix_yaw(desired_position)
-        elif state_==1:
-            go_straight_ahead(desired_position)
-        elif state_==2:
+        if state_ == 0:
+            fix_yaw(desired_position_)
+        elif state_ == 1:
+            go_straight_ahead(desired_position_)
+        elif state_ == 2:
             done()
-            pass 
+            pass
         else:
-            rospy.logerr('unkonwn state')
-            pass       
-    rate.sleep()
-
-
-
+            rospy.logerr('Unknown state!')
+            pass
+        rate.sleep()
 
 if __name__ == '__main__':
     main()
